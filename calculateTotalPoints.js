583d6767cffdf46750d16a8f2df60335e874d078
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-15 13:49:33 +0200

    Fix empty lines

--- calculateTotalPoints.js
+++ calculateTotalPoints.js
@@ -6,11 +6,6 @@ const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, windFactor
     const stylePoints = calculateStylePoints(styleNotes);
     const totalPoints = Math.round(distancePoints * 10 + stylePoints * 10 + windFactor * 10 + gateFactor * 10) / 10
 
-
     return (!isNaN(totalPoints) ? totalPoints : 'Wrong data');
-
-
-
 }
-
 module.exports = calculateTotalPoints;
